e596a745a3108662ef00d82cb7bf1a3f4bc5223c
yousef sama <ys2152000@gmail.com>
2024-03-18 08:31:29 +0200

    create type adaptive for notemodel

--- lib/Models/Notes_Model.dart
+++ lib/Models/Notes_Model.dart
@@ -1,7 +1,16 @@
-class NotesModel {
+import 'package:hive/hive.dart';
+
+part "Notes_Model.g.dart";
+
+@HiveType(typeId: 0)
+class NotesModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subtitle;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
 
   NotesModel(
