4bbce47633729dcacd582246d52321e634c27421
yousef sama <ys2152000@gmail.com>
2024-03-18 08:00:39 +0200

    add hive no sql database

---
+++ lib/Models/NotesModel.dart
@@ -0,0 +1,12 @@
+class NotesModel {
+  final String title;
+  final String subtitle;
+  final String date;
+  final int color;
+
+  NotesModel(
+      {required this.title,
+      required this.subtitle,
+      required this.date,
+      required this.color});
+}
