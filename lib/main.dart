5395b5d45a9afdbb52334c23f559555a6d01e823
yousef sama <ys2152000@gmail.com>
2024-03-18 10:01:14 +0200

    init Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:noteapp/Views/Home_View.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+import 'package:noteapp/Views/Home_View.dart';
+import 'package:noteapp/constant.dart';
+
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
   runApp(const NoteApp());
 }
 
